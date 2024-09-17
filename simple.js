bc2880fde642a63b0560d7460a57e20c92241dd1
andreeich <andreei.chervoniak@gmail.com>
2024-09-17 18:57:48 +0300

    animated cube rotation with drawArrays function

--- simple.js
+++ simple.js
@@ -9,28 +9,36 @@ if (!gl) {
 }
 
 const vertexData = [
-  0,
-  1,
-  0, // v1.position
-  -1,
-  -1,
-  0, // v2.position
-  1,
-  -1,
-  0, // v3.position
+  // Front face
+  1, 1, 1, 1, -1, 1, -1, 1, 1, -1, 1, 1, 1, -1, 1, -1, -1, 1,
+  // Back face
+  1, 1, -1, 1, -1, -1, -1, 1, -1, -1, 1, -1, 1, -1, -1, -1, -1, -1,
+  // Top face
+  1, 1, 1, 1, 1, -1, -1, 1, 1, -1, 1, 1, 1, 1, -1, -1, 1, -1,
+  // Bottom face
+  1, -1, 1, 1, -1, -1, -1, -1, 1, -1, -1, 1, 1, -1, -1, -1, -1, -1,
+  // Right face
+  1, 1, 1, 1, 1, -1, 1, -1, 1, 1, -1, 1, 1, 1, -1, 1, -1, -1,
+  // Left face
+  -1, 1, 1, -1, 1, -1, -1, -1, 1, -1, -1, 1, -1, 1, -1, -1, -1, -1,
 ];
 
 const colorData = [
-  1,
-  0,
-  0, // v1.color
-  0,
-  1,
-  0, // v2.color
-  0,
-  0,
-  1, // v3.color
+  [1.0, 0.0, 0.0], // Front face (red)
+  [0.0, 1.0, 0.0], // Back face (green)
+  [0.0, 0.0, 1.0], // Top face (blue)
+  [1.0, 1.0, 0.0], // Bottom face (yellow)
+  [1.0, 0.0, 1.0], // Right face (magenta)
+  [0.0, 1.0, 1.0], // Left face (cyan)
 ];
+
+let colors = [];
+
+for (let i = 0; i < 6; i++) {
+  const c = colorData[i];
+  // Each face consists of 2 triangles, and each triangle has 3 vertices
+  colors = colors.concat(c, c, c, c, c, c);
+}
 
 const positionBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
@@ -38,7 +46,7 @@ gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
 
 const colorBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorData), gl.STATIC_DRAW);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors.flat()), gl.STATIC_DRAW);
 
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(
@@ -86,25 +94,26 @@ gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
 gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
 
 gl.useProgram(program);
+gl.enable(gl.DEPTH_TEST);
 
 const uniformLocations = {
   matrix: gl.getUniformLocation(program, "matrix"),
 };
 
 const matrix = mat4.create();
-mat4.translate(matrix, matrix, [0.2, 0.3, 0]);
-mat4.scale(matrix, matrix, [0.5, 0.4, 0]);
+mat4.scale(matrix, matrix, [0.5, 0.5, 0.5]);
 
 function animate() {
   requestAnimationFrame(animate);
   mat4.rotateZ(matrix, matrix, Math.PI / 100);
   mat4.rotateX(matrix, matrix, Math.PI / 100);
+  mat4.rotateY(matrix, matrix, Math.PI / 100);
   gl.uniformMatrix4fv(uniformLocations.matrix, false, matrix);
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  gl.drawArrays(gl.TRIANGLES, 0, vertexData.length / 3);
 }
 
 gl.uniformMatrix4fv(uniformLocations.matrix, false, matrix);
 
-gl.drawArrays(gl.TRIANGLES, 0, 3);
+gl.drawArrays(gl.TRIANGLES, 0, vertexData.length / 3);
 
 animate();
